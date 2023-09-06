3b6c454a3e09899100e9f792b2cb49821e6560a9
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-09-06 13:50:45 +0300

    first migration created meals.db database

---
+++ app/migrations/versions/2602c104cd36_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 2602c104cd36
+Revises: 
+Create Date: 2023-09-06 13:48:58.110725
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2602c104cd36'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- app/models.py
+++ app/models.py
@@ -1,2 +1,14 @@
 from sqlalchemy import Column,Integer,String,ForeignKey,desc
 from base import Base,session
+
+class Recipe(Base):
+    __tablename__='recipes'
+
+    id=Column(Integer(),primary_key=True)
+    name=Column(String())
+    category=Column(String())
+    instructions=Column(String())
+
+    def __repr__(self):
+        return f"Recipe: {self.name} , Category: {self.category} , Instructions: {self.instructions}"
+    
