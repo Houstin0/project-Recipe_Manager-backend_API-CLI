c286231f55b7d254d3b8ff46daff2960ae43e630
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-09-08 00:57:55 +0300

    crud fully added and search functionalities

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from models import *
 import click
 
@@ -11,7 +13,8 @@ def cli():
 @click.option('--time_taken',prompt="Time Taken",help="number of greetings")   
 @click.option('--instructions',prompt="Recipe Instructions",help="number of greetings")   
 def add_recipe(name,category,time_taken,instructions):
-        """Function to add an Recipe by name"""
+        """Add an new Recipe"""
+
         recipe=Recipe(
         name=name,
         category=category,
@@ -28,7 +31,8 @@ cli.add_command(add_recipe)
 @click.option('--start_date',prompt="Meal Plan start date",help="number of greetings")   
 @click.option('--end',prompt="Meal Plan end date",help="number of greetings")   
 def add_meal_plan(name,start_date,end):
-     """Function to add a Meal plan by name"""
+     """Add a new Meal plan """
+
      meal_plan=Meal_plan(
           name=name,
           start_date=start_date,
@@ -44,7 +48,8 @@ cli.add_command(add_meal_plan)
 @click.option('--recipe_name',prompt="Recipe Name",help="number of greetings")  
 @click.option('--meal_plan_name',prompt="Meal Plan Name",help="number of greetings")  
 def add_ingredient(ingredient_name,recipe_name,meal_plan_name):
-     """Function to add an ingredient"""
+     """Add an new ingredient"""
+     
      recipe_obj=session.query(Recipe.id).filter(Recipe.name==recipe_name).first()
      meal_plan_obj=session.query(Meal_plan.id).filter(Meal_plan.name==meal_plan_name).first()
      
@@ -65,10 +70,11 @@ def add_ingredient(ingredient_name,recipe_name,meal_plan_name):
      click.echo(f"Added {ingredient_name} to Ingredients")
 cli.add_command(add_ingredient)
 
+
 @click.command()
 @click.option('--recipe_name',prompt="Recipe Name to Delete")
 def delete_recipe(recipe_name):
-     """Function to delete a Recipe by name"""
+     """Delete a Recipe by name"""
      recipe_to_delete=session.query(Recipe).filter(Recipe.name==recipe_name).first()
 
      if not recipe_to_delete:
@@ -82,7 +88,7 @@ cli.add_command(delete_recipe)
 @click.command()
 @click.option('--meal_plan_name', prompt="Meal Plan Name to Delete", help="Meal plan name to delete")
 def delete_meal_plan(meal_plan_name):
-    """Function to delete a Meal Plan by name"""
+    """Delete a Meal Plan by name"""
     meal_plan = session.query(Meal_plan).filter(Meal_plan.name == meal_plan_name).first()
 
     if not meal_plan:
@@ -97,7 +103,7 @@ cli.add_command(delete_meal_plan)
 @click.command()
 @click.option('--ingredient_name', prompt="Ingredient Name to Delete", help="Ingredient name to delete")
 def delete_ingredient(ingredient_name):
-    """Function to delete an ingredient by name"""
+    """Delete an ingredient by name"""
 
     ingredient = session.query(Ingredient).filter(Ingredient.name == ingredient_name).first()
 
@@ -108,12 +114,114 @@ def delete_ingredient(ingredient_name):
     session.delete(ingredient)
     session.commit()
     click.echo(f"Deleted {ingredient_name} from Ingredients") 
-cli.add_command(delete_ingredient)  
+cli.add_command(delete_ingredient) 
+
+@click.command()
+def delete_all_meal_plans():
+    """deletes all from Meal Plans table"""
+    session.query(Meal_plan).delete()
+    session.commit()
+    click.echo("Deleted all records from the Meal Plans table.")
+cli.add_command(delete_all_meal_plans)
+
+
+@click.command()
+@click.option('--recipe_name', prompt="Recipe Name to Update", help="Recipe name to update")
+@click.option('--column', prompt="Column to Update (name, category, time_taken, instructions)", help="Column to update")
+@click.option('--new_value', prompt="New Value", help="New value for the column")
+def update_recipe_column(recipe_name, column, new_value):
+    """Update a column of a recipe"""
+
+    recipe = session.query(Recipe).filter(Recipe.name == recipe_name).first()
+
+    if not recipe:
+        click.echo(f"Recipe '{recipe_name}' not found.")
+        return
+
+    if column == 'name':
+        recipe.name = new_value
+    elif column == 'category':
+        recipe.category = new_value
+    elif column == 'time_taken':
+        recipe.time_taken = new_value
+    elif column == 'instructions':
+        recipe.instructions = new_value
+    else:
+        click.echo("Invalid column name. Available columns: name, category, time_taken, instructions.")
+        return
+
+    session.commit()
+    click.echo(f"Updated {column} of recipe '{recipe_name}'")
+cli.add_command(update_recipe_column)    
+
+@click.command()
+@click.option('--meal_plan_name', prompt="Name of Meal Plan to Update", help="Meal plan name to update")
+@click.option('--column', prompt="Column to Update (name, start_date, end)", help="Column to update")
+@click.option('--new_value', prompt="New Value", help="New value for the column")
+def update_meal_plan_column(meal_plan_name, column, new_value):
+    """Update a column of a meal plan"""
+
+    meal_plan = session.query(Meal_plan).filter(Meal_plan.name == meal_plan_name).first()
+
+    if not meal_plan:
+        click.echo(f"Meal plan '{meal_plan_name}' not found.")
+        return
+
+    if column == 'name':
+        meal_plan.name = new_value
+    elif column == 'start_date':
+        meal_plan.start_date = new_value
+    elif column == 'end':
+        meal_plan.end = new_value
+    else:
+        click.echo("Invalid column name. Available columns: name, start_date, end.")
+        return
+
+    session.commit()
+    click.echo(f"Updated {column} of meal plan '{meal_plan_name}'")
+cli.add_command(update_meal_plan_column)  
+
+@click.command()
+@click.option('--ingredient_name', prompt="Ingredient Name to Update", help="Ingredient name to update")
+@click.option('--column', prompt="Column to Update (name, recipe, meal_plan)", help="Column to update")
+@click.option('--new_value', prompt="New Value", help="New value for the column")
+def update_ingredient_column(ingredient_name, column, new_value):
+    """Update a column of an ingredient"""
+
+    ingredient = session.query(Ingredient).filter(Ingredient.name == ingredient_name).first()
+
+    if not ingredient:
+        click.echo(f"Ingredient '{ingredient_name}' not found.")
+        return
+
+    if column == 'name':
+        ingredient.name = new_value
+    elif column == 'recipe':
+        recipe = session.query(Recipe).filter(Recipe.name == new_value).first()
+        if not recipe:
+            click.echo(f"Recipe '{new_value}' not found.")
+            return
+        ingredient.recipe_id = recipe.id
+    elif column == 'meal_plan':
+        meal_plan = session.query(Meal_plan).filter(Meal_plan.name == new_value).first()
+        if not meal_plan:
+            click.echo(f"Meal plan '{new_value}' not found.")
+            return
+        ingredient.meal_plan_id = meal_plan.id
+    else:
+        click.echo("Invalid column name. Available columns: name, recipe, meal_plan.")
+        return
+
+    session.commit()
+    click.echo(f"Updated {column} of ingredient '{ingredient_name}'")
+cli.add_command(update_ingredient_column)
+
+
 
 @click.command()
 @click.option('--recipe_name', prompt="Recipe Name to Search", help="Recipe name to search for")
 def search_recipe(recipe_name):
-    """Function to search for recipes by name"""
+    """search for recipes by name"""
 
     recipes = session.query(Recipe).filter(Recipe.name.ilike(f"%{recipe_name}%")).all()
 
@@ -129,7 +237,8 @@ cli.add_command(search_recipe)
 @click.command()
 @click.option('--meal_plan_name', prompt="Meal Plan Name to Search", help="Meal plan name to search for")
 def search_meal_plan(meal_plan_name):
-    """Function to search for meal plans by name"""
+    """search for meal plans by name"""
+
     meal_plans = session.query(Meal_plan).filter(Meal_plan.name.ilike(f"%{meal_plan_name}%")).all()
 
     if not meal_plans:
@@ -144,7 +253,8 @@ cli.add_command(search_meal_plan)
 @click.command()
 @click.option('--ingredient_name', prompt="Ingredient Name to Search", help="Ingredient name to search for")
 def search_ingredient(ingredient_name):
-    """Function to search for ingredients by name"""
+    """search for ingredients by name"""
+
     ingredients = session.query(Ingredient).filter(Ingredient.name.ilike(f"%{ingredient_name}%")).all()
 
     if not ingredients:
@@ -176,7 +286,7 @@ cli.add_command(all_recipes)
 
 @click.command()
 def all_meal_plans():
-    """Function display all meal plans"""
+    """display all meal plans"""
 
     meal_plans = session.query(Meal_plan).all()
 
@@ -194,7 +304,7 @@ cli.add_command(all_meal_plans)
 
 @click.command()
 def all_ingredients():
-    """Function to display all ingredients"""
+    """display all ingredients"""
 
     ingredients = session.query(Ingredient).all()
 
@@ -211,12 +321,51 @@ def all_ingredients():
 cli.add_command(all_ingredients)
 
 
+
+
 @click.command()
-@click.option('--recipe_name', prompt="Recipe Name to Update", help="Recipe name to update")
-@click.option('--column', prompt="Column to Update (name, category, time_taken, instructions)", help="Column to update")
-@click.option('--new_value', prompt="New Value", help="New value for the column")
-def update_recipe_column(recipe_name, column, new_value):
-    """Function to update a specific column of a recipe by name"""
+@click.option('--category', prompt="Recipe Category to Retrieve", help="Recipe category to retrieve")
+def get_recipes_by_category(category):
+    """get recipes by category"""
+    recipes = session.query(Recipe).filter(Recipe.category.ilike(f"%{category}%")).all()
+
+    if not recipes:
+        click.echo(f"No recipes found in the '{category}' category.")
+        return
+
+    click.echo(f"Recipes in the '{category}' category:")
+    for recipe in recipes:
+        click.echo(f"- Name: {recipe.name}")
+        click.echo(f"  Category: {recipe.category}")
+        click.echo(f"  Time Taken: {recipe.time_taken}")
+        click.echo(f"  Instructions: {recipe.instructions}")
+        click.echo()
+
+cli.add_command(get_recipes_by_category)
+
+
+@click.command()
+@click.option('--start_date', prompt="Meal Plan Start Date to Retrieve", help="Meal plan start date to retrieve")
+def get_meal_plans_by_start_date(start_date):
+    """get meal plans by start date"""
+    meal_plans = session.query(Meal_plan).filter(Meal_plan.start_date == start_date).all()
+
+    if not meal_plans:
+        click.echo(f"No meal plans found with the start date '{start_date}'.")
+        return
+
+    click.echo(f"Meal plans with start date '{start_date}':")
+    for meal_plan in meal_plans:
+        click.echo(f"- Name: {meal_plan.name}")
+        click.echo(f"  Start Date: {meal_plan.start_date}")
+        click.echo(f"  End Date: {meal_plan.end}")
+        click.echo()
+cli.add_command(get_meal_plans_by_start_date)
+ 
+@click.command()
+@click.option('--recipe_name', prompt="Recipe Name to Retrieve Ingredients From", help="Recipe name to retrieve ingredients from")
+def get_ingredients_by_recipe_name(recipe_name):
+    """get ingredients by recipe name"""
 
     recipe = session.query(Recipe).filter(Recipe.name == recipe_name).first()
 
@@ -224,86 +373,100 @@ def update_recipe_column(recipe_name, column, new_value):
         click.echo(f"Recipe '{recipe_name}' not found.")
         return
 
-    # Update the specified column with the new value
-    if column == 'name':
-        recipe.name = new_value
-    elif column == 'category':
-        recipe.category = new_value
-    elif column == 'time_taken':
-        recipe.time_taken = new_value
-    elif column == 'instructions':
-        recipe.instructions = new_value
-    else:
-        click.echo("Invalid column name. Available columns: name, category, time_taken, instructions.")
+    
+    ingredients = session.query(Ingredient).filter(Ingredient.recipe_id == recipe.id).all()
+
+    if not ingredients:
+        click.echo(f"No ingredients found for recipe '{recipe_name}'.")
         return
 
-    session.commit()
-    click.echo(f"Updated {column} of recipe '{recipe_name}'")
-cli.add_command(update_recipe_column)    
-
+    click.echo(f"Ingredients for recipe '{recipe_name}':")
+    for ingredient in ingredients:
+        click.echo(f"- Name: {ingredient.name}")
+        click.echo(f"  Meal Plan: {ingredient.meal_plan.name if ingredient.meal_plan else 'N/A'}")
+        click.echo()
+cli.add_command(get_ingredients_by_recipe_name)
+ 
 @click.command()
-@click.option('--meal_plan_name', prompt="Meal Plan Name to Update", help="Meal plan name to update")
-@click.option('--column', prompt="Column to Update (name, start_date, end)", help="Column to update")
-@click.option('--new_value', prompt="New Value", help="New value for the column")
-def update_meal_plan_column(meal_plan_name, column, new_value):
-    """Function to update a specific column of a meal plan by name"""
+@click.option('--meal_plan_name', prompt="Meal Plan Name to Retrieve Ingredients From", help="Meal plan name to retrieve ingredients from")
+def get_ingredients_by_meal_plan_name(meal_plan_name):
+    """get ingredients by meal plan name"""
 
+   
     meal_plan = session.query(Meal_plan).filter(Meal_plan.name == meal_plan_name).first()
 
     if not meal_plan:
         click.echo(f"Meal plan '{meal_plan_name}' not found.")
         return
 
-    # Update the specified column with the new value
-    if column == 'name':
-        meal_plan.name = new_value
-    elif column == 'start_date':
-        meal_plan.start_date = new_value
-    elif column == 'end':
-        meal_plan.end = new_value
-    else:
-        click.echo("Invalid column name. Available columns: name, start_date, end.")
+  
+    ingredients = session.query(Ingredient).filter(Ingredient.meal_plan_id == meal_plan.id).all()
+
+    if not ingredients:
+        click.echo(f"No ingredients found for meal plan '{meal_plan_name}'.")
         return
 
-    session.commit()
-    click.echo(f"Updated {column} of meal plan '{meal_plan_name}'")
-cli.add_command(update_meal_plan_column)  
+    click.echo(f"Ingredients for meal plan '{meal_plan_name}':")
+    for ingredient in ingredients:
+        click.echo(f"- Name: {ingredient.name}")
+        click.echo(f"  Recipe: {ingredient.recipe.name if ingredient.recipe else 'N/A'}")
+        click.echo()
+cli.add_command(get_ingredients_by_meal_plan_name)
 
 @click.command()
-@click.option('--ingredient_name', prompt="Ingredient Name to Update", help="Ingredient name to update")
-@click.option('--column', prompt="Column to Update (name, recipe, meal_plan)", help="Column to update")
-@click.option('--new_value', prompt="New Value", help="New value for the column")
-def update_ingredient_column(ingredient_name, column, new_value):
-    """Function to update a specific column of an ingredient by name"""
-    ingredient = session.query(Ingredient).filter(Ingredient.name == ingredient_name).first()
+@click.option('--meal_plan_name', prompt="Meal Plan Name to Retrieve Recipes From", help="Meal plan name to retrieve recipes from")
+def get_recipes_by_meal_plan(meal_plan_name):
+    """get recipes by meal plan name"""
+    meal_plan = session.query(Meal_plan).filter(Meal_plan.name == meal_plan_name).first()
 
-    if not ingredient:
-        click.echo(f"Ingredient '{ingredient_name}' not found.")
+    if not meal_plan:
+        click.echo(f"Meal plan '{meal_plan_name}' not found.")
         return
 
-    # Update the specified column with the new value
-    if column == 'name':
-        ingredient.name = new_value
-    elif column == 'recipe':
-        recipe = session.query(Recipe).filter(Recipe.name == new_value).first()
-        if not recipe:
-            click.echo(f"Recipe '{new_value}' not found.")
-            return
-        ingredient.recipe_id = recipe.id
-    elif column == 'meal_plan':
-        meal_plan = session.query(Meal_plan).filter(Meal_plan.name == new_value).first()
-        if not meal_plan:
-            click.echo(f"Meal plan '{new_value}' not found.")
-            return
-        ingredient.meal_plan_id = meal_plan.id
-    else:
-        click.echo("Invalid column name. Available columns: name, recipe, meal_plan.")
+    recipes = session.query(Recipe).join(Ingredient).filter(Ingredient.meal_plan_id == meal_plan.id).all()
+
+    if not recipes:
+        click.echo(f"No recipes found for meal plan '{meal_plan_name}'.")
         return
 
-    session.commit()
-    click.echo(f"Updated {column} of ingredient '{ingredient_name}'")
-cli.add_command(update_ingredient_column)
+    click.echo(f"Recipes for meal plan '{meal_plan_name}':")
+    for recipe in recipes:
+        click.echo(f"- Name: {recipe.name}")
+        click.echo(f"  Category: {recipe.category}")
+        click.echo(f"  Time Taken: {recipe.time_taken}")
+        click.echo(f"  Instructions: {recipe.instructions}")
+        click.echo()
+cli.add_command(get_recipes_by_meal_plan)
 
-     
+@click.command()
+@click.option('--meal_plan_name', prompt="Meal Plan Name", help="Meal plan name")
+@click.option('--recipe_category', prompt="Recipe Category", help="Recipe category")
+def get_recipes_by_meal_plan_and_category(meal_plan_name, recipe_category):
+    """get recipes by meal plan and category"""
+    meal_plan = session.query(Meal_plan).filter(Meal_plan.name == meal_plan_name).first()
+
+    if not meal_plan:
+        click.echo(f"Meal plan '{meal_plan_name}' not found.")
+        return
+
+    recipes = session.query(Recipe).join(Ingredient).filter(
+        Ingredient.meal_plan_id == meal_plan.id,
+        Recipe.category.ilike(f"%{recipe_category}%")
+    ).all()
+
+    if not recipes:
+        click.echo(f"No recipes found for meal plan '{meal_plan_name}' and category '{recipe_category}'.")
+        return
+
+    click.echo(f"Recipes for meal plan '{meal_plan_name}' in category '{recipe_category}':")
+    for recipe in recipes:
+        click.echo(f"- Name: {recipe.name}")
+        click.echo(f"  Category: {recipe.category}")
+        click.echo(f"  Time Taken: {recipe.time_taken}")
+        click.echo(f"  Instructions: {recipe.instructions}")
+        click.echo()
+cli.add_command(get_recipes_by_meal_plan_and_category)
+    
+
 if __name__=='__main__':
     cli()    
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from sqlalchemy import create_engine,Column,Integer,String,ForeignKey
 from sqlalchemy.orm import sessionmaker,relationship
 from sqlalchemy.ext.declarative import declarative_base
